1b73597b334d36812d2f02e1c633e29a0c2ac0c6
Hiroshi <mnakasone3@my.bcit.ca>
2025-10-27 21:00:36 -0700

    server.js fixes for render

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const mongoStore = require('./database/connections/mongoStoreConnection');
-
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
